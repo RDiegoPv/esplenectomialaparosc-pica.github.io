990dbd480656c6960f4819821f91ff4b405f2377
RDiegoPv <157375929+RDiegoPv@users.noreply.github.com>
2026-03-15 18:52:14 -0500

    Add smooth scrolling for navigation links

--- esplenectomia-web/js/script.js
+++ esplenectomia-web/js/script.js
@@ -1,1 +1,13 @@
+const links = document.querySelectorAll("nav a");
 
+links.forEach(link => {
+link.addEventListener("click", e=>{
+e.preventDefault();
+const section = document.querySelector(link.getAttribute("href"));
+
+window.scrollTo({
+top: section.offsetTop - 60,
+behavior:"smooth"
+});
+});
+});
