76a9309ac91b13b5270b148dffd17786f8e8aa80
RDiegoPv <157375929+RDiegoPv@users.noreply.github.com>
2026-03-15 18:59:38 -0500

    Adjust scroll offset for smooth scrolling

--- esplenectomia-web/js/script.js
+++ esplenectomia-web/js/script.js
@@ -1,13 +1,18 @@
 const links = document.querySelectorAll("nav a");
 
 links.forEach(link => {
+
 link.addEventListener("click", e=>{
+
 e.preventDefault();
+
 const section = document.querySelector(link.getAttribute("href"));
 
 window.scrollTo({
-top: section.offsetTop - 60,
+top: section.offsetTop - 50,
 behavior:"smooth"
 });
+
 });
+
 });
